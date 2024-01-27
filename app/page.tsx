1729b840b021633353090ed32415acd3e3656e90
Rushdi J <rushdi@trendflow.se>
2024-01-27 02:58:39 +0100

    Mobile padding fix on home

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default async function Home({
   const pokemonListData = await getPokemonList({ limit, page });
 
   return (
-    <main className="p-24 pt-32 grow">
+    <main className="p-12 md:p-24 pt-12 md:pt-24 lg:pt-32 grow">
       <section className="w-full items-center justify-between text-center py-6">
-        <h1>Welcome to Kantodex</h1>
+        <h1 className="text-4xl md:text-5xl">Welcome to Kantodex</h1>
         <h3>Your trusted source for information about Pokemon from Generation 1.</h3>
       </section>
-      <section className="w-full flex justify-center py-6">
+      <section className="w-full flex justify-center py-3 md:py-6">
         <PokemonList listData={pokemonListData} page={page} />
       </section>
     </main>
